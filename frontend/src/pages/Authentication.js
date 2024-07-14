6c326cf37dd6af0ba8708f35c080c6afb3b50c62
Kushal Niraula <kushal463334@gmail.com>
2024-07-14 20:22:40 +1000

    Authentication applied successfully!

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -1,7 +1,38 @@
-import AuthForm from '../components/AuthForm';
+import AuthForm from "../components/AuthForm";
+import { json, redirect } from "react-router-dom";
 
 function AuthenticationPage() {
   return <AuthForm />;
 }
 
 export default AuthenticationPage;
+export async function action({ request }) {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get("mode") || "login";
+  if (mode !== "login" && mode !== "signup") {
+    return json({ message: "Unsupported mode!", status: 422 });
+  }
+  const data = await request.formData();
+  const authData = {
+    email: data.get("email"),
+    password: data.get("password"),
+  };
+  const response = await fetch("http://localhost:8080/" + mode, {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify(authData),
+  });
+  if (response.status === 422 || response.status === 401) {
+    return response;
+  }
+  if (!response.ok) {
+    throw json({ message: "could not authenticate user", status: 500 });
+  }
+  const resData = await response.json();
+  const token = resData.token;
+  const expiration = new Date();
+  expiration.setSeconds(expiration.getSeconds() + 10);
+  localStorage.setItem("token", token);
+  localStorage.setItem("expiration", expiration.toISOString());
+  return redirect("/");
+}
